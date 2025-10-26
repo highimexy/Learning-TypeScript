f5064ac071f65c5b6c24c1e7da94d732a80e2622
highimexy <teslawiktor@gmail.com>
2025-10-26 12:55:43 +0100

    16:52

---
+++ New tutorial/index.js
@@ -0,0 +1,39 @@
+const menu = [
+  { name: "Margharita", price: 8 },
+  { name: "Pepperoni", price: 10 },
+  { name: "Hawaiian", price: 10 },
+  { name: "Veggie", price: 9 },
+];
+
+const cashInRegister = 100;
+const nextOrderId = 1;
+const orderQueue = [];
+
+function addNewPizza(pizzaObj) {
+  menu.push(pizzaObj);
+}
+
+function placeOrder(pizzaName) {
+  const orderedPizza = menu.find((pizzaObj) => pizzaObj.name === pizzaName);
+  cashInRegister += orderedPizza.price;
+  const newOrder = {id: nextOrderId++, pizza: orderedPizza, status:"ordered"};
+  orderQueue.push(newOrder);
+  return newOrder;
+}
+
+function completeOrder(orderId){
+    const order = orderQueue.find(order => order.id === orderId);
+    order.status = "completed";
+    return order;
+}
+
+addNewPizza({name: "Chicken", cost: 12})
+addNewPizza({name: "BBQ Chicken", cost: 12})
+addNewPizza({name: "Spicy Chicken", cost: 12})
+
+placeOrder("Chicken")
+completeOrder("1")
+
+console.log("Menu:", menu);
+console.log("Cash in register:", cashInRegister);
+console.log("Order queue:", orderQueue);
