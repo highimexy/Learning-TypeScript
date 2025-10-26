6df32ad81a261bac63899751e49401d6ab3d448a
highimexy <teslawiktor@gmail.com>
2025-10-26 08:26:49 +0100

    1h:02 Funkcje

--- myFunctions.ts
+++ myFunctions.ts
@@ -1,5 +1,30 @@
 function addTwo(num: number) {
   return num + 2;
 }
-
 addTwo(2);
+
+let value2 = addTwo(2) + addTwo('leter')
+
+function getUpper(val: string) {
+  return val.toUpperCase();
+}
+getUpper("String");
+
+export {};
+
+function signUpUser(
+  name: string,
+  email: string,
+  isPaid: boolean,
+  age: number
+) {}
+
+signUpUser("string", "string", true, 24);
+
+let loginUser = (
+  name: string,
+  email: string,
+  isPaid: boolean = false,
+  age: number = 24
+) => {};
+loginUser("w","EMAIL@GMAIL.COM");
