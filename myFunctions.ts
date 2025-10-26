4770fc49fecd3f91d3f7bd39b4abcd7aba08f467
highimexy <teslawiktor@gmail.com>
2025-10-26 12:14:22 +0100

    1h:16 Funkcje v2

--- myFunctions.ts
+++ myFunctions.ts
@@ -1,17 +1,18 @@
-function addTwo(num: number) {
-  return num + 2;
+//simple function
+function addTwo(num: number): number {
+  //return num + 2;
+  return "string";
 }
 addTwo(2);
 
-let value2 = addTwo(2) + addTwo('leter')
-
+// simple function
 function getUpper(val: string) {
   return val.toUpperCase();
 }
 getUpper("String");
 
 export {};
-
+//function with few params
 function signUpUser(
   name: string,
   email: string,
@@ -21,10 +22,39 @@ function signUpUser(
 
 signUpUser("string", "string", true, 24);
 
+//function with pre assigned values
 let loginUser = (
   name: string,
   email: string,
   isPaid: boolean = false,
   age: number = 24
 ) => {};
-loginUser("w","EMAIL@GMAIL.COM");
+loginUser("w", "EMAIL@GMAIL.COM");
+
+// Kilka możliwych return
+function getValue(myVal: number): boolean | string {
+  if (myVal > 5) {
+    return true;
+  }
+  return "200 OK";
+}
+let result1 = getValue(4);
+let result2 = getValue(7);
+
+console.log(result1, result2);
+
+//ARRAY
+const heroes = ["thor", "spiderman", "ironman"];
+
+heroes.map((hero) => {
+  return `Hero is ${hero}`
+});
+
+//return nothing
+function consoleError(errmsg: string): void{
+  console.log(errmsg);
+}
+//never return
+function handleError(msg: string): never{
+  throw new Error(msg)
+}
