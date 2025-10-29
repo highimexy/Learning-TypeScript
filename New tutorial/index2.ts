993607cb6524b7bbfc7776b1918ea8d5318d8a0b
highimexy <teslawiktor@gmail.com>
2025-10-29 19:49:02 +0100

    1h:18 TS-specific types: any

--- New tutorial/index2.ts
+++ New tutorial/index2.ts
@@ -1,3 +1,29 @@
-import { GetPizzaDetail } from "./index.ts";
+type UserRole = "guest" | "member" | "admin";
 
-console.log(GetPizzaDetail(1))
+type User = {
+  username: string;
+  role: UserRole;
+};
+
+const users: User[] = [
+  {
+    username: "John_doe",
+    role: "admin",
+  },
+  {
+    username: "Jane_Malik",
+    role: "member",
+  },
+  {
+    username: "guest_user",
+    role: "guest",
+  },
+];
+
+function fetchUserDetails(username: string): User {
+  const user = users.find((user) => user.username === username);
+  if (!user) {
+    throw new Error(`User with username ${username} not found`);
+  }
+  return user;
+}
